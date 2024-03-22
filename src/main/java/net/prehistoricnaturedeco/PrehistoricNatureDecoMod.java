2d8a20e89f9b44034a521c3478ae264578ee96d1
AechtRob <Aecht_Rob@outlook.com>
2024-03-22 09:54:35 +0000

    v.2 (for main mod v.60)

--- src/main/java/net/prehistoricnaturedeco/PrehistoricNatureDecoMod.java
+++ src/main/java/net/prehistoricnaturedeco/PrehistoricNatureDecoMod.java
@@ -30,7 +30,7 @@ import java.util.function.Supplier;
 public class PrehistoricNatureDecoMod {
 	public static final String MODID = "prehistoricnaturedeco";
 	public static final String NAME = "Prehistoric Nature Decorations";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "2.0";
 	public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("pndeco:a");
     @SidedProxy(clientSide = "net.prehistoricnaturedeco.ClientProxyPrehistoricNatureDecoMod", serverSide = "net.prehistoricnaturedeco.ServerProxyPrehistoricNatureDecoMod")
 	public static IProxyPrehistoricNatureDecoMod proxy;
