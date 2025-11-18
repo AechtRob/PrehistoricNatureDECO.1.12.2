cb7d4cc3baadb6f0595a31f1dbc5d4e77666c530
AechtRob <Aecht_Rob@outlook.com>
2025-11-18 18:32:46 +0000

    v.3.01 new woods and sittability fix

---
+++ src/main/java/net/prehistoricnaturedeco/block/BlockBiarmobaieraPlanter2.java
@@ -0,0 +1,43 @@
+
+package net.prehistoricnaturedeco.block;
+
+import net.lepidodendron.LepidodendronSorter;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.prehistoricnaturedeco.block.base.BlockPlanter;
+
+@net.prehistoricnaturedeco.ElementsPrehistoricNatureDecoMod.ModElement.Tag
+public class BlockBiarmobaieraPlanter2 extends net.prehistoricnaturedeco.ElementsPrehistoricNatureDecoMod.ModElement {
+	@GameRegistry.ObjectHolder("lepidodendron:biarmobaiera_planter2")
+	public static final Block block = null;
+	public BlockBiarmobaieraPlanter2(net.prehistoricnaturedeco.ElementsPrehistoricNatureDecoMod instance) {
+		super(instance, LepidodendronSorter.biarmobaiera_planter2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("lepidodendron:biarmobaiera_planter2"));
+		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
+				new ModelResourceLocation("lepidodendron:biarmobaiera_planter2", "inventory"));
+	}
+
+	public static class BlockCustom extends BlockPlanter {
+		public BlockCustom() {
+			setTranslationKey("pf_biarmobaiera_planter2");
+		}
+	}
+
+}
